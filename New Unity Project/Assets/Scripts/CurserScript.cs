ea778695fcfbbd87f6baf52f09f803ba1a47c5b4
Max <maxmanenschijn@gmail.com>
2019-09-05 14:46:58 +0200

    Did the basic dialog system. Ready for tomorrow MVP.

---
+++ New Unity Project/Assets/Scripts/CurserScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurserScript : MonoBehaviour
+{
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = true;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public TMP_Text dialogueText;
+    public MonoBehaviour firstPersonController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        //fpsEnable = firstPersonController.GetComponent(typeof(ScriptableObject));
+        Debug.Log("Staring conversation with: " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        firstPersonController.enabled = false;
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialog()
+    {
+        Debug.Log("Ending");
+        firstPersonController.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        //firstPersonController.SetActive(true);
+    }
+}
