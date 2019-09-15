0cfd00500b349a6805e23e2739fb92d33f084192
ChiefMax <maxmanenschijn@gmail.com>
2019-09-15 16:28:04 +0200

    Did the glowing objects.

---
+++ New Unity Project/Assets/Scripts/GlowHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Behaviour halo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        halo.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        halo.enabled = false;
+    }
+}
